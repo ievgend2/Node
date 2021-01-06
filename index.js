9fb1fc94ce212a4a7f74bd750b2a77fdd9801f9d
ievgend2 <ievgend2@gmail.com>
2021-01-06 01:48:04 -0500

    server is UP

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // (function (exports, require, module, __dirname, __filename) {
-
+const fs = require('fs')
+const path = require('path')
 // const chalk = require('chalk')
 // const text = require('./data')
 
@@ -10,3 +11,31 @@
 // console.log(__filename)
 
 const http = require('http')
+const server = http.createServer((req, res) => {
+
+    if (req.url === '/') {
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
+            if (err) {
+                throw err
+            }
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            })
+            res.end(data)
+        })
+    }
+    else if (req.url === '/contact') {
+        fs.readFile(path.join(__dirname, 'public', 'contact.html'), (err, data) => {
+            if (err) {
+                throw err
+            }
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            })
+            res.end(data)
+        })
+    }
+})
+server.listen(3000, () => {
+    console.log("Server is started...")
+})
