ce33b7fb457e7eaabd05b9a6569946a3187b5f70
ievgend2 <ievgend2@gmail.com>
2021-01-06 02:15:02 -0500

    server is UP & Running

--- index.js
+++ index.js
@@ -13,29 +13,73 @@ const path = require('path')
 const http = require('http')
 const server = http.createServer((req, res) => {
 
-    if (req.url === '/') {
-        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
-            if (err) {
-                throw err
-            }
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            })
-            res.end(data)
-        })
+    // if (req.url === '/') {
+    //     fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
+    //         if (err) {
+    //             throw err
+    //         }
+    //         res.writeHead(200, {
+    //             'Content-Type': 'text/html'
+    //         })
+    //         res.end(data)
+    //     })
+    // }
+    // else if (req.url === '/contact') {
+    //     fs.readFile(path.join(__dirname, 'public', 'contact.html'), (err, data) => {
+    //         if (err) {
+    //             throw err
+    //         }
+    //         res.writeHead(200, {
+    //             'Content-Type': 'text/html'
+    //         })
+    //         res.end(data)
+    //     })
+    // }
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url)
+    const ext = path.extname(filePath)
+    let contentType = 'text/html'
+
+    switch (ext) {
+        case '.css':
+            contentType = 'text/css'
+            break
+        case '.css':
+            contentType = 'text/javascript'
+            break
+        default:
+            contentType = 'text/html'
     }
-    else if (req.url === '/contact') {
-        fs.readFile(path.join(__dirname, 'public', 'contact.html'), (err, data) => {
-            if (err) {
-                throw err
-            }
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            })
-            res.end(data)
-        })
+
+    if (!ext) {
+        filePath += '.html'
     }
+    // console.log(filePath)
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            fs.readFile(path.join(__dirname, 'public', 'error.html'), (err, data) => {
+                if (err) {
+                    res.writeHead(500)
+                    res.end('Error')
+                } else {
+                    res.writeHead(200, {
+                        'Content-Type': 'text/html'
+                    })
+                    res.end(data)
+                }
+            })
+        } else {
+            res.writeHead(200, {
+                'Content-Type': contentType
+            })
+            res.end(content)
+        }
+
+    })
 })
+
+const PORT = process.env.PORT || 3000
+
+
 server.listen(3000, () => {
-    console.log("Server is started...")
+    console.log(`Server is started on ${PORT}...`)
 })
