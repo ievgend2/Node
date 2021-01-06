0f0861fe750e17980903b7773a2b84101947cc0d
ievgend2 <ievgend2@gmail.com>
2021-01-06 01:06:27 -0500

    added os.js & events.js

--- demo/fs.js
+++ demo/fs.js
@@ -1,6 +1,7 @@
 // File System
 const fs = require('fs')
 const path = require('path')
+const { fileURLToPath } = require('url')
 
 // fs.mkdir(path.join(__dirname, "test"), (err) => {
 //     if (err) {
@@ -9,3 +10,21 @@ const path = require('path')
 //     console.log("Dir Created Successfully")
 // })
 
+const filePath = path.join(__dirname, "test", "text.txt")
+
+// fs.appendFile(filePath, "\nHello Again from Text", err => {
+//     if (err) {
+//         throw err
+//     }
+//     console.log("Dir Created Successfully")
+
+// })
+
+fs.readFile(filePath, "utf-8", (err, content) => {
+    if (err) {
+        throw err
+    }
+    // const data = Buffer.from(content)
+    // console.log("content", data.toString())
+    console.log(content)
+})
---
+++ demo/os.js
@@ -0,0 +1,18 @@
+
+
+const os = require('os')
+
+console.log("OS name", os.platform())
+console.log("Architecture of chipset", os.arch())
+console.log("Chipset info", os.cpus())
+console.log("Free Memory size", os.freemem())
+console.log("Total Memory size", os.totalmem())
+
+console.log("Home dir", os.homedir())
+console.log("Runing time", os.uptime())
+
+
+
+
+
+
