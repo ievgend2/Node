fee0f6022e8affa980c7274c57ed5f9f14b179b8
ievgend2 <ievgend2@gmail.com>
2021-01-06 00:53:55 -0500

    modified fs

--- demo/fs.js
+++ demo/fs.js
@@ -1,2 +1,11 @@
 // File System
-const fs = require('fs') 
+const fs = require('fs')
+const path = require('path')
+
+// fs.mkdir(path.join(__dirname, "test"), (err) => {
+//     if (err) {
+//         throw err
+//     }
+//     console.log("Dir Created Successfully")
+// })
+
